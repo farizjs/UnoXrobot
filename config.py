6a73faf700c19d113df730ea2bda1c73d4deb13a
FJ_GAMING <84852749+fjgaming212@users.noreply.github.com>
2021-11-25 12:24:47 +0700

    Update config.py

--- config.py
+++ config.py
@@ -20,16 +20,16 @@
 
 import json
 
-with open("app.json","r") as f:
+with open("config.json","r") as f:
     config = json.loads(f.read())
 
-TOKEN=config.get("token")
-WORKERS=config.get("workers", 32)
-ADMIN_LIST = config.get("admin_list", None)
-OPEN_LOBBY = config.get("open_lobby", True)
-ENABLE_TRANSLATIONS = config.get("enable_translations", False)
-DEFAULT_GAMEMODE = config.get("default_gamemode", "fast")
-WAITING_TIME = config.get("waiting_time", 120)
-TIME_REMOVAL_AFTER_SKIP = config.get("time_removal_after_skip", 20)
-MIN_FAST_TURN_TIME = config.get("min_fast_turn_time", 15)
-MIN_PLAYERS = config.get("min_players", 2)
+TOKEN = 2125839096:AAEquwngQspHnA9emK8G_CC6Uh-JszqgYpU
+WORKERS = 32
+ADMIN_LIST = 1514078508
+OPEN_LOBBY = True
+ENABLE_TRANSLATIONS = False
+DEFAULT_GAMEMODE = fast
+WAITING_TIME = 120
+TIME_REMOVAL_AFTER_SKIP = 20
+MIN_FAST_TURN_TIME = 15
+MIN_PLAYERS = 5
